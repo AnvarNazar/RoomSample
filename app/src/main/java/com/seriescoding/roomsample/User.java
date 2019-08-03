de43349170349de312731556d4d0cc5ed2788f87
Anvar Nazar <anvarnasar@ymail.com>
2019-08-03 20:57:49 +0530

    create entity

---
+++ app/src/main/java/com/seriescoding/roomsample/User.java
@@ -0,0 +1,21 @@
+package com.seriescoding.roomsample;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+
+    @ColumnInfo(name = "email")
+    public String email;
+}
