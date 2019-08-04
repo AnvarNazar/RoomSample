0c21f26a2b7b93363fcd7cb80a15e822532430dc
Anvar Nazar <anvarnasar@ymail.com>
2019-08-04 13:59:40 +0530

    update AppDatabase

--- app/src/main/java/com/seriescoding/roomsample/AppDatabase.java
+++ app/src/main/java/com/seriescoding/roomsample/AppDatabase.java
@@ -3,7 +3,16 @@ package com.seriescoding.roomsample;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {
+        User.class,
+        Address.class,
+        Book.class},
+
+        version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+
+    public abstract AddressDao addressDao();
+
+    public abstract BookDao bookDao();
 }
