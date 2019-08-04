be983ffaa94a74702faa2693f6776a0062a7e5bc
Anvar Nazar <anvarnasar@ymail.com>
2019-08-04 13:52:24 +0530

    add AddressDao

---
+++ app/src/main/java/com/seriescoding/roomsample/AddressDao.java
@@ -0,0 +1,30 @@
+package com.seriescoding.roomsample;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface AddressDao {
+
+    @Insert
+    void insert(Address address);
+
+    @Insert
+    void insertAll(Address... addresses);
+
+    @Query("SELECT * FROM address WHERE id = :id")
+    Address loadById(int id);
+
+    @Query("SELECT * FROM address WHERE id IN (:ids)")
+    Address loadAllByIds(int[] ids);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void update(Address address);
+
+    @Delete
+    void delete(Address address);
+}
